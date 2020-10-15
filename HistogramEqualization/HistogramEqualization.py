1042d4950d004b4cebbf90b28bc99881154acf83
Nguyen Ngoc Huy <nh01272639408@gmail.com>
2020-10-15 11:29:48 +0700

    Add new origin image + result image

--- HistogramEqualization/HistogramEqualization.py
+++ HistogramEqualization/HistogramEqualization.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('anhthieusang.jpg')
+img = cv2.imread('Originpicture.jpg')
+img = cv2.resize(img,(600,500))
 HSVimg = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 h,s,v = cv2.split(HSVimg)
 v = cv2.equalizeHist(v)
